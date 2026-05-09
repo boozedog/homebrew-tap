de754818551aa0b83a26ca13657b9f5fcd8d7125
boozedog <code@booze.dog>
2026-05-09 06:57:15 -0400

    bump smoovmux to 0.0.5

--- Casks/smoovmux.rb
+++ Casks/smoovmux.rb
@@ -1,6 +1,6 @@
 cask "smoovmux" do
   version "0.0.5"
-  sha256 :no_check
+  sha256 "745d15e540639d8bbb0f9b970a0192c2e67eee1251889b38aa44178f16a048f7"
 
   url "https://github.com/boozedog/smoovmux/releases/download/v#{version}/smoovmux-#{version}-macos-universal.zip"
   name "smoovmux"
